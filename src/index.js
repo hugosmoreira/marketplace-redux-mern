0d57a998be690305ea4f7fe9f72872d1c5409f4d
Hugo Moreira <hugo.n4@gmail.com>
2023-03-13 22:45:55 -0700

    boilerplate

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ConfigProvider } from 'antd';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+    theme={{
+      components: {
+        Button: {
+          colorPrimary: "#40513B",
+          colorPrimaryHover: "#40513B",
+          borderRadius: "2px",
+          boxShadow: "none",
+        },
+      },
+    }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
